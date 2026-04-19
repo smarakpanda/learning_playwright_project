2775c498f60dc6e3aa2ddbf2c50892211064603b
smarakpanda <smarakpanda81@gmail.com>
2026-04-19 09:42:45 +0530

    Fixed the config path in config reader

--- utils/configReader.py
+++ utils/configReader.py
@@ -1,27 +1,16 @@
-import configparser
-
-from utils.decorators.LoggingDecorator import log_decorator
 import os
-
+import configparser
 
 class ConfigReader:
 
     def __init__(self):
-
-        print("CWD:", os.getcwd())
-        print("Files in CWD:", os.listdir())
-
         self.config = configparser.ConfigParser()
-        self.config.read(r"C:\Users\smara\PycharmProjects\PythonProject\config.properties")
 
-    @log_decorator
-    def get_value_from_config(self,key,section="DEFAULT"):
-        return self.config[section][key]
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        project_root = os.path.abspath(os.path.join(base_dir, "..", ".."))
+        config_path = os.path.join(project_root, "config.ini")
 
+        print("Reading config from:", config_path)
+        print("Exists?", os.path.exists(config_path))
 
-if __name__ == "__main__":
-    config = ConfigReader()
-    print(config.get_value_from_config("headless",))
-
-
-
+        self.config.read(config_path)
