54b38eca3e5f66d38460b652c6dcd45d6643d61c
smarakpanda <smarakpanda81@gmail.com>
2026-04-19 09:36:53 +0530

    added a code for resolvong browser issue

--- utils/configReader.py
+++ utils/configReader.py
@@ -1,11 +1,16 @@
 import configparser
 
 from utils.decorators.LoggingDecorator import log_decorator
+import os
 
 
 class ConfigReader:
 
     def __init__(self):
+
+        print("CWD:", os.getcwd())
+        print("Files in CWD:", os.listdir())
+
         self.config = configparser.ConfigParser()
         self.config.read(r"C:\Users\smara\PycharmProjects\PythonProject\config.properties")
 
